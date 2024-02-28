c5ba0e49713c882e9f55a90228dcc63ab3e8ab02
leonel1414 <LEO@DESKTOP-ER0GBFR>
2024-02-27 23:21:26 -0300

    Nuevo ejercicios de agenda electronica

---
+++ src/agendaelectronica/AgendaElectronica.java
@@ -0,0 +1,15 @@
+package agendaelectronica;
+
+import agendaelectronica.igu.Principal;
+
+public class AgendaElectronica {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Principal principal = new Principal();
+        
+        principal.setVisible(true);
+        principal.setLocationRelativeTo(null);
+    }
+
+}
